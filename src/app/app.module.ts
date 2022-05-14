df36ffe71049c9306ffcca3a822d7680921d4c0f
Joan Morales <josmoralesp0307@outlook.es>
2022-05-14 15:23:00 -0500

    Comienzo de proyecto Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/login-register/login-register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-register',
+  templateUrl: './login-register.component.html',
+  styles: [
+  ]
+})
+export class LoginRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
