889afd1e766508bed2a717b67e5edb3a827e7028
Joan Morales <josmoralesp0307@outlook.es>
2022-05-14 15:37:56 -0500

    Enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 
 const routes: Routes = [
   {
-    
+    path: '',
+    component: HomeComponentComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login-register',
+    component: LoginRegisterComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
 
